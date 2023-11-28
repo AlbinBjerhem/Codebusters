aa0b049526716bfea8f4ab2e19df1fdb2d97672f
AccidentByDragon <karl17loe@hotmail.com>
2023-11-28 09:53:34 +0100

    börjat Mäklare Inloggning

--- public/MäklareSida/MaklareInloggning.js
+++ public/MäklareSida/MaklareInloggning.js
@@ -1,6 +1,20 @@
+// import users stuff here
+
 const tempUsers = [
   {
     username: "admin",
     password: "admin"
   }
 ]
+
+const sectionLogin = document.createElement("section");
+sectionLogin.id = "loginEstateAgent";
+const sectionloginH2 = document.createElement("h2");
+sectionloginH2.innerText = "Estate Agent Login";
+sectionLogin.appendChild(sectionloginH2);
+
+const loginForm = document.createElement("form");
+loginForm.id = "loginForm";
+const loginFormUsernameLabel = document.createElement("label");
+loginFormUsernameLabel.innerText = "Username: ";
+const loginUsernameInput = document.createElement("input");
