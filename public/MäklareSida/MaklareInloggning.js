da3dc773319c6bce30b684ccc32447d89a7fc1f4
AccidentByDragon <karl17loe@hotmail.com>
2023-11-30 10:13:38 +0100

    tabller
    tabeller för förslag och sölj bostäder

--- public/MäklareSida/MaklareInloggning.js
+++ public/MäklareSida/MaklareInloggning.js
@@ -9,12 +9,15 @@ const tempUsers = [
 ]
 
 const tempHousesForSale = [
-
+  {
+    Address: "23 Happylane",
+    Agent: "Karl",
+  }
 ]
 
 const tempHousesSuggested = [
   {
-    Address: "23 Happylane",
+    Address: "24 Happylane",
     Agent: "",
   }
 ]
@@ -65,13 +68,58 @@ const logOut = document.createElement("input");
 logOut.type = "submit";
 logOut.value = "Log Out";
 
-//arbeta med sälj lägenheter här
+// Föreslagna lägenheter/object
+const unclaimedPropertiesTable = document.createElement("table");
+const uPTHead = document.createElement("thead");
+const uPTBody = document.createElement("thead");
+const uPTRowHead = document.createElement("tr");
+const uPThAdress = document.createElement("th");
+const uPThInfo = document.createElement("th"); // Knapp för att få ut mer info?
 
+uPThAdress.innerText = "Property Adress"
+uPThInfo.innerText = "Knapp för Info"
+
+uPTRowHead.appendChild(uPThAdress);
+uPTRowHead.appendChild(uPThInfo);
+uPTHead.appendChild(uPTRowHead);
+unclaimedPropertiesTable.appendChild(uPTHead);
+unclaimedPropertiesTable.appendChild(uPTBody);
+
+//arbeta med sälj lägenheter/object här
+const sellPropertiesTable = document.createElement("table");
+const sPTHead = document.createElement("thead");
+const sPTBody = document.createElement("thead");
+const sPTRowHead = document.createElement("tr");
+const sPThAdress = document.createElement("th");
+const sPTAgent = document.createElement("th");
+
+sPThAdress.innerText = "Property Adress";
+sPTAgent.innerText = "Current Agent";
+
+sPTRowHead.appendChild(sPThAdress);
+sPTRowHead.appendChild(sPTAgent);
+sPTHead.appendChild(sPTRowHead);
+sellPropertiesTable.appendChild(sPTHead);
+sellPropertiesTable.appendChild(sPTBody);
+
+for (let house of tempHousesForSale) {
+  const houseRow = document.createElement("tr");
+  const houseAdress = document.createElement("td");
+  const houseAgent = document.createElement("td");
+
+  houseAdress.innerText = house.Address;
+  houseAgent.innerText = house.Agent;
+
+  houseRow.appendChild(houseAdress);
+  houseRow.appendChild(houseAgent);
+  sPTBody.appendChild(houseRow);
+}
 // två tabeller en med hem tagna av mäklare en utan?
 // skulle en tabell fungera?
 
 logOutForm.appendChild(logOut)
 sectionLoggedIn.appendChild(sectionLoggedInh2);
+sectionLoggedIn.appendChild(sellPropertiesTable);
 sectionLoggedIn.appendChild(logOutForm);
 document.body.appendChild(sectionLoggedIn);
 
