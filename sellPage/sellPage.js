16b7e3cbef207ed689a0ebf7c343e39b4bbb3b44
AlbinBjerhem <albinbjerhem@gmail.com>
2023-11-30 06:48:02 +0100

    update

--- sellPage/sellPage.js
+++ sellPage/sellPage.js
@@ -1,7 +1,45 @@
-var myAppDiv = document.getElementById("myApp");
+document.addEventListener("DOMContentLoaded", function () {
+  let form = document.createElement("form");
 
-var heading = document.createElement("h1");
-heading.textContent = "Hello World!";
+  let streetInput = createInput("text", "Gatunamn");
+  let houseNumberInput = createInput("text", "Husnummer");
+  let cityInput = createInput("text", "Stadsdel");
+  let zipCodeInput = createInput("text", "Postkod");
 
-myAppDiv.appendChild(heading);
-myAppDiv.appendChild()
+  const submitButton = document.createElement("button");
+  submitButton.type = "button";
+  submitButton.textContent = "Submit";
+
+  let displayDiv = document.createElement("div");
+  displayDiv.id = "displayData";
+
+  form.appendChild(streetInput);
+  form.appendChild(houseNumberInput);
+  form.appendChild(cityInput);
+  form.appendChild(zipCodeInput);
+  form.appendChild(submitButton);
+
+  document.getElementById("myApp").appendChild(form);
+  document.getElementById("myApp").appendChild(displayDiv);
+
+  submitButton.addEventListener("click", function () {
+    let streetValue = streetInput.value;
+    let houseNumberValue = houseNumberInput.value;
+    let cityValue = cityInput.value;
+    let zipCodeValue = zipCodeInput.value;
+
+    displayDiv.innerHTML = `
+      <p>Street Name: ${streetValue}</p>
+      <p>House Number: ${houseNumberValue}</p>
+      <p>City: ${cityValue}</p>
+      <p>ZIP Code: ${zipCodeValue}</p>
+    `;
+  });
+});
+
+function createInput(type, placeholder) {
+  const input = document.createElement("input");
+  input.type = type;
+  input.placeholder = placeholder;
+  return input;
+}
