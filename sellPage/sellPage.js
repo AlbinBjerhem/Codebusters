9f1c7d8beab863508642f104515bf83235a8784a
AlbinBjerhem <albinbjerhem@gmail.com>
2023-12-05 11:30:06 +0100

    Lagt till byggår

--- sellPage/sellPage.js
+++ sellPage/sellPage.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
   let typeOfPropertyInput = createDropdown("Typ av bostad", ["Lägenhet", "Radhus", "Villa"]);
   let roomAmountInput = createDropdown("Antal rum", [1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15]);
   let areaInput = createInput("number", "Boarea (m²)");
+  let creationYearInput = createYearDropdown("Byggår");
 
   const submitButton = document.createElement("button");
   submitButton.type = "button";
@@ -23,6 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
   form.appendChild(typeOfPropertyInput);
   form.appendChild(roomAmountInput);
   form.appendChild(areaInput);
+  form.appendChild(creationYearInput);
   form.appendChild(submitButton);
 
   document.getElementById("myApp").appendChild(form);
@@ -36,6 +38,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let typeOfPropertyValue = typeOfPropertyInput.value;
     let roomAmountValue = roomAmountInput.value;
     let areaValue = areaInput.value;
+    let creationYearValue = creationYearInput.value;
 
     displayDiv.innerHTML = `
       <p>Gatunamn: ${streetValue}</p>
@@ -45,6 +48,7 @@ document.addEventListener("DOMContentLoaded", function () {
       <p>Typ av bostad: ${typeOfPropertyValue}</p>
       <p>Antal rum: ${roomAmountValue}</p>
       <p>Boarea: ${areaValue} m²</p>
+      <p>Byggår: ${creationYearValue}</p>
     `;
   });
 });
@@ -69,4 +73,22 @@ function createDropdown(placeholder, options) {
 
   return select;
 
+}
+
+function createYearDropdown(placeholder) {
+  const select = document.createElement("select");
+  const defaultOption = document.createElement("option");
+  defaultOption.value = "";
+  defaultOption.text = placeholder;
+  select.appendChild(defaultOption);
+
+  const currentYear = new Date().getFullYear();
+  for (let year = currentYear; year >= 1850; year--) {
+    const option = document.createElement("option");
+    option.value = year.toString();
+    option.text = year.toString();
+    select.appendChild(option);
+  }
+
+  return select;
 }
