dc1f48ce232b0ee7770af465e96ef0b042f9b2f5
AlexandraFilipsson <alexandrafilipsson@gmail.com>
2023-12-05 15:15:41 +0100

    removed json
    made the code itself work - removed json - will do it sep.

---
+++ sellPage.js
@@ -0,0 +1,36 @@
+function selectOption(category) {
+  var dropdown = document.getElementById(category);
+  var selectedOption = dropdown.options[dropdown.selectedIndex].value;
+  console.log(category + ": " + selectedOption);
+}
+
+function createDropdown(labelText, id) {
+  var container = document.createElement("div");
+  var label = document.createElement("label");
+  label.textContent = labelText;
+  container.appendChild(label);
+
+  var select = document.createElement("select");
+  select.id = id;
+  select.onchange = function () {
+    selectOption(id);
+  };
+
+  var options = ["Ja", "Nej"];
+  for (var i = 0; i < options.length; i++) {
+    var option = document.createElement("option");
+    option.value = options[i];
+    option.textContent = options[i];
+    select.appendChild(option);
+  }
+
+  container.appendChild(select);
+  document.body.appendChild(container);
+  document.body.appendChild(document.createElement("br"));
+}
+
+createDropdown("Hiss:", "hissOptions");
+createDropdown("Parkering:", "parkeringOptions");
+createDropdown("Innergård:", "innergardOptions");
+createDropdown("Förråd:", "forradOptions");
+createDropdown("Vind:", "vindOptions");
--- sellPage/public/server-request.js
+++
@@ -1,8 +0,0 @@
-export async function getAllVindOptions() {
-
-  const response = await fetch("/hissOptions")
-  console.log("Respons - ", response)
-  const result = response.json()
-  console.log("Result -", result)
-  return result
-}
