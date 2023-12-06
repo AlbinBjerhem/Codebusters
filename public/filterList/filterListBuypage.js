6791a11222fa713bf65cbab27ff1243a0bd84f7f
AccidentByDragon <karl17loe@hotmail.com>
2023-12-06 11:05:27 +0100

    setup 2

--- public/filterList/filterListBuypage.js
+++ public/filterList/filterListBuypage.js
@@ -1,3 +1,27 @@
 const listToFilter = [
-  
+  {
+    adress: "1 hissvägen",
+    elevator: true
+  },
+  {
+    adress: "1 trappvägen",
+    elevator: false
+  }
 ]
+
+const filteringList = false;
+
+
+
+
+function changeElementVisibility(elementID) {
+  if (elementID.style.display == "none") {
+    elementID.style.display = "block";
+  }
+  else if (elementID.style.display == "block") {
+    elementID.style.display = "none";
+  }
+  else {
+    console.log(`cannot change the display type of ${elementID}`)
+  }
+}
