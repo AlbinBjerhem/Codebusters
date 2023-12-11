0e673ce2bb6066d979fc573fc0c2559d61e6dbeb
AlbinBjerhem <albinbjerhem@gmail.com>
2023-12-11 14:53:34 +0100

    Fått Alexandras kod att fungera, dock ej bra

--- sellPage/sellPage.js
+++ sellPage/sellPage.js
@@ -100,14 +100,16 @@ function createYearDropdown(placeholder) {
 
   return select;
 }
-/*
+
+//----------------------------------------------- Alexandras kod ----------------------------------------------------------
+
 function selectOption(category) {
   var dropdown = document.getElementById(category);
   var selectedOption = dropdown.options[dropdown.selectedIndex].value;
   console.log(category + ": " + selectedOption);
 }
 
-function createDropdown(labelText, id) {
+function createDropdownMenu(labelText, id) {
   var container = document.createElement("div");
   var label = document.createElement("label");
   label.textContent = labelText;
@@ -132,10 +134,8 @@ function createDropdown(labelText, id) {
   document.body.appendChild(document.createElement("br"));
 }
 
-createDropdown("Hiss:", "hissOptions");
-createDropdown("Parkering:", "parkeringOptions");
-createDropdown("Innergård:", "innergardOptions");
-createDropdown("Förråd:", "forradOptions");
-createDropdown("Vind:", "vindOptions");
-
-*/
+createDropdownMenu("Hiss:", "hissOptions");
+createDropdownMenu("Parkering:", "parkeringOptions");
+createDropdownMenu("Innergård:", "innergardOptions");
+createDropdownMenu("Förråd:", "forradOptions");
+createDropdownMenu("Vind:", "vindOptions");
