f796a89e8504ad9c1ee9387b50f17a5cf1f67a08
AlexandraFilipsson <alexandrafilipsson@gmail.com>
2023-12-12 16:01:22 +0100

    testdata input
    new files for testing

---
+++ buyPage/test.js
@@ -0,0 +1,75 @@
+// Sample data
+var numbers = [5, 2, 8, 1, 9];
+
+// Function to create and display the sorted list
+function createAndDisplayList() {
+  // Get the reference to the <div> element with id "myApp"
+  var myAppDiv = document.getElementById('myApp');
+
+  // Create an <ul> element
+  var ulElement = document.createElement('ul');
+
+  // Iterate through the numbers and create <li> elements
+  numbers.forEach(function (number) {
+    var liElement = document.createElement('li');
+    liElement.textContent = number;
+    ulElement.appendChild(liElement);
+  });
+
+  // Append the <ul> element to the <div>
+  myAppDiv.appendChild(ulElement);
+
+  // Create a dropdown with sorting options
+  createDropdown();
+}
+// Function to create a dropdown with sorting options
+function createDropdown() {
+  var sortingOptions = [
+    { value: 'asc', text: 'Sort Ascending' },
+    { value: 'desc', text: 'Sort Descending' }
+  ];
+
+  var selectElement = document.createElement('select');
+
+  sortingOptions.forEach(function (option) {
+    var optionElement = document.createElement('option');
+    optionElement.value = option.value;
+    optionElement.textContent = option.text;
+    selectElement.appendChild(optionElement);
+  });
+
+  var buttonElement = document.createElement('button');
+  buttonElement.textContent = 'Sort';
+  buttonElement.addEventListener('click', function () {
+    var selectedOption = selectElement.value;
+    sortList(selectedOption);
+  });
+
+  var myAppDiv = document.getElementById('myApp');
+  myAppDiv.appendChild(selectElement);
+  myAppDiv.appendChild(buttonElement);
+}
+
+// Function to sort the list and update the display
+function sortList(order) {
+  // Sort the array based on the order
+  if (order === 'asc') {
+    numbers.sort(function (a, b) {
+      return a - b;
+    });
+  } else if (order === 'desc') {
+    numbers.sort(function (a, b) {
+      return b - a;
+    });
+  }
+
+  // Clear the existing list
+  var myAppDiv = document.getElementById('myApp');
+  myAppDiv.innerHTML = '';
+
+  // Call the function to create and display the sorted list
+  createAndDisplayList();
+}
+
+// Call the function to create and display the initial list
+createAndDisplayList();
