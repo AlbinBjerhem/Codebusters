bb3db2d97a6590ef9223e94758f3af259eb2c68c
AccidentByDragon <karl17loe@hotmail.com>
2023-12-13 10:14:38 +0100

    Update filterListBuypage.js

--- public/buyPage/filterList/filterListBuypage.js
+++ public/buyPage/filterList/filterListBuypage.js
@@ -21,21 +21,29 @@ export default function filterList() {
   filterChbx1.type = "checkbox";
   filterChbx1.name = "Elevator Access";
   filterChbx1.value = "Elevator";
+  const filterChBx2Label = document.createElement("label")
+  filterChBx2Label.innerText = "Garage Access: "
   const filterChbx2 = document.createElement("input")
   filterChbx2.id = "filterGarage"
   filterChbx2.type = "checkbox";
   filterChbx2.name = "Garage Access";
   filterChbx2.value = "Garage";
+  const filterChBx3Label = document.createElement("label")
+  filterChBx3Label.innerText = "Garden Access: "
   const filterChbx3 = document.createElement("input")
   filterChbx3.id = "filterGarden"
   filterChbx3.type = "checkbox";
   filterChbx3.name = "Garden Access";
   filterChbx3.value = "Garden";
+  const filterChBx4Label = document.createElement("label")
+  filterChBx4Label.innerText = "Storage Access: "
   const filterChbx4 = document.createElement("input")
   filterChbx4.id = "filterStorage"
   filterChbx4.type = "checkbox";
   filterChbx4.name = "Storage Access";
   filterChbx4.value = "Storage";
+  const filterChBx5Label = document.createElement("label")
+  filterChBx5Label.innerText = "Attic Storage Access: "
   const filterChbx5 = document.createElement("input")
   filterChbx5.id = "filterAtticStorage"
   filterChbx5.type = "checkbox";
@@ -76,6 +84,14 @@ export default function filterList() {
 
   formFilterCriteria.appendChild(filterChBx1Label);
   formFilterCriteria.appendChild(filterChbx1);
+  formFilterCriteria.appendChild(filterChBx2Label);
+  formFilterCriteria.appendChild(filterChbx2);
+  formFilterCriteria.appendChild(filterChBx3Label);
+  formFilterCriteria.appendChild(filterChbx3);
+  formFilterCriteria.appendChild(filterChBx4Label);
+  formFilterCriteria.appendChild(filterChbx4);
+  formFilterCriteria.appendChild(filterChBx5Label);
+  formFilterCriteria.appendChild(filterChbx5);
   formFilterCriteria.appendChild(filterSubmit);
   sectionFilterCriteria.appendChild(formFilterCriteria);
   sectionFilterCriteria.appendChild(tableFilterTable)
@@ -85,9 +101,6 @@ export default function filterList() {
   sectionFilterCriteria.addEventListener("submit", applyFilter)  
   return sectionFilterCriteria
 }
-
-
-
 
 function changeElementVisibility(elementID) {
   if (elementID.style.display == "none") {
